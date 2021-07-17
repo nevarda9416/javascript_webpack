6c67dcbe10589e12f32038516ec3d5cfbea6758c
tudao <tudao@5nhatnhat.com>
2021-07-18 00:59:52 +0700

    - [Webpack]: Asset management loading images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // Add the image to our existing div
+    const myIocn = new Image();
+    myIocn.src = Icon;
+    element.appendChild(myIocn);
+
     return element;
 }
 document.body.appendChild(component());
